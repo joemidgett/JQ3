c8cf992b04bbb3113c122e92fb787f35586a6692
Joe Midgett <joe@joemidgett.com>
2022-02-08 12:42:10 -0800

    Create shape for bypass buttons

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -64,6 +64,48 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
     }
 }
 
+void LookAndFeel::drawToggleButton(juce::Graphics& g,
+    juce::ToggleButton& toggleButton,
+    bool shouldDrawButtonAsHighlighted,
+    bool shouldDrawButtonAsDown)
+{
+    using namespace juce;
+
+    Path powerButton;
+
+    auto bounds = toggleButton.getLocalBounds();
+
+    //g.setColour(Colours::red);
+    //g.drawRect(bounds);
+
+    auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 6;
+    auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+
+    float ang = 30.f;
+
+    size -= 6;
+
+    powerButton.addCentredArc(r.getCentreX(), 
+        r.getCentreY(), 
+        size * 0.5, 
+        size * 0.5, 
+        0.f, 
+        degreesToRadians(ang), 
+        degreesToRadians(360.f - ang),
+        true);
+
+    powerButton.startNewSubPath(r.getCentreX(), r.getY());
+    powerButton.lineTo(r.getCentre());
+
+    PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
+
+    auto color = toggleButton.getToggleState() ? Colours::dimgrey : Colour(0u, 172u, 1u);
+
+    g.setColour(color);
+    g.strokePath(powerButton, pst);
+    g.drawEllipse(r, 2);
+}
+
 // ==============================================================================
 void RotarySliderWithLabels::paint(juce::Graphics& g)
 {
@@ -566,12 +608,18 @@ JQ3AudioProcessorEditor::JQ3AudioProcessorEditor(JQ3AudioProcessor& p)
         addAndMakeVisible(comp);
     }
 
+    peakBypassButton.setLookAndFeel(&lnf);
+    lowcutBypassButton.setLookAndFeel(&lnf);
+    highcutBypassButton.setLookAndFeel(&lnf);
+
     setSize(600, 480);
 }
 
 JQ3AudioProcessorEditor::~JQ3AudioProcessorEditor()
 {
-
+    peakBypassButton.setLookAndFeel(nullptr);
+    lowcutBypassButton.setLookAndFeel(nullptr);
+    highcutBypassButton.setLookAndFeel(nullptr);
 }
 
 //==============================================================================
