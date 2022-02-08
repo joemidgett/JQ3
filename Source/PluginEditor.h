b0bc10acee6a4a3fc574b9aa74e35a7df9dd1f48
Joe Midgett <joe@joemidgett.com>
2022-02-08 13:52:58 -0800

    Fix freq path toggle button on mouseover

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -255,8 +255,33 @@ private:
 };
 
 //==============================================================================
-struct PowerButton : juce::ToggleButton { };
-struct AnalyzerButton : juce::ToggleButton { };
+struct PowerButton : juce::ToggleButton 
+{ 
+    
+};
+
+struct AnalyzerButton : juce::ToggleButton 
+{ 
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+
+        randomPath.clear();
+
+        juce::Random r;
+
+        randomPath.startNewSubPath(insetRect.getX(), insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+
+        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
+        {
+            // Should pre-cache Y value, but can do that later
+            randomPath.lineTo(x, insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+    }
+
+    juce::Path randomPath;
+};
 
 
 class JQ3AudioProcessorEditor : public juce::AudioProcessorEditor
